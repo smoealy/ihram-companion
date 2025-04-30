254e08133b89b58e38f80a74497d935ab0b03812
smoealy <129239220+smoealy@users.noreply.github.com>
2025-04-30 16:34:49 -0400

    Create index.ts

---
+++ src/firebase/index.ts
@@ -0,0 +1,14 @@
+// src/firebase/index.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
