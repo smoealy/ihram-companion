e4cb0e69ebcb5ab418f4b0d0a778b04797c5a688
smoealy <129239220+smoealy@users.noreply.github.com>
2025-04-30 16:40:24 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
-// vite.config.ts
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import path from "path"
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['firebase/app', 'firebase/firestore'],
-  },
-  build: {
-    rollupOptions: {
-      external: [],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // ✅ This is required!
     },
   },
 })
